767feae8fa83b71c08766662fde12dc61dec1fb5
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-10-21 09:52:02 +0300

    JpaRepository interface

---
+++ src/main/java/com/shevchenkostas77/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.shevchenkostas77.spring.springboot.spring_data_jpa.dao;
+
+import com.shevchenkostas77.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
---
+++ src/main/java/com/shevchenkostas77/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.shevchenkostas77.spring.springboot.spring_data_jpa.service;
+
+import com.shevchenkostas77.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.shevchenkostas77.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String name) {
+        List<Employee> employee = employeeRepository.findAllByName(name);
+
+        return employee;
+    }
+}
